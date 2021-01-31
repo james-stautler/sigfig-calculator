4d260478579e1e753ffc83234e9cf2d4d583daea
james-stautler <74434990+james-stautler@users.noreply.github.com>
2021-01-31 12:55:09 -0600

    basic foundation

--- sigfig.py
+++ sigfig.py
@@ -1,1 +1,18 @@
+class Number:
+    def __init__(self, val):
+        self.val = val
+
+    def get_val(self):
+        return self.val
+
+    def is_decimal(self):
+        if isinstance(self.get_val(), float):
+            return True
+        else:
+            return False
+
+
+x = Number(20.0)
+
+print(x.is_decimal())
 
